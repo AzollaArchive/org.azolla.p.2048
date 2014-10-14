31d70ef8d0d8c0605cc624cd2da3020543f17558
Azolla <github@azolla.org>
2014-10-14 21:59:11 +0800

    init

---
+++ src/main/java/org/azolla/p/tzfe/panel/TzfePanel.java
@@ -0,0 +1,37 @@
+/*
+ * @(#)TzfePanel.java		Created at 2014年10月14日
+ * 
+ * Copyright (c) 2011-2014 azolla.org All rights reserved.
+ * Azolla PROPRIETARY/CONFIDENTIAL. Use is subject to license terms. 
+ */
+package org.azolla.p.tzfe.panel;
+
+import javax.swing.JFrame;
+
+/**
+ * The coder is very lazy for this TzfePanel class
+ *
+ * @author 	sk@azolla.org
+ * @since 	ADK1.0
+ */
+public class TzfePanel extends JFrame
+{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 7581667051127144281L;
+
+    private TzfePanel         instance;
+
+    public TzfePanel single()
+    {
+        return instance = null == instance ? new TzfePanel() : instance;
+    }
+
+    private TzfePanel()
+    {
+
+    }
+
+}
---
+++ src/main/java/org/azolla/p/tzfe/startup/Startup.java
@@ -0,0 +1,29 @@
+/*
+ * @(#)Startup.java		Created at 2014年10月14日
+ * 
+ * Copyright (c) 2011-2014 azolla.org All rights reserved.
+ * Azolla PROPRIETARY/CONFIDENTIAL. Use is subject to license terms. 
+ */
+package org.azolla.p.tzfe.startup;
+
+/**
+ * The coder is very lazy for this Startup class
+ *
+ * @author 	sk@azolla.org
+ * @since 	ADK1.0
+ */
+public class Startup
+{
+
+    /**
+     * The coder is very lazy for this main method
+     * 
+     * @param args void
+     */
+    public static void main(String[] args)
+    {
+        //do nothing
+
+    }
+
+}
