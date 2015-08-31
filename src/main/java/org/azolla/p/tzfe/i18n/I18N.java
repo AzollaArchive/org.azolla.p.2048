b28a375b16178fdbfdae9af6a2709a904f57056a
Azolla <github@azolla.org>
2015-08-31 21:49:40 +0800

    Url0 ref log4j

--- src/main/java/org/azolla/p/tzfe/i18n/I18N.java
+++ src/main/java/org/azolla/p/tzfe/i18n/I18N.java
@@ -11,6 +11,7 @@ import org.azolla.l.ling.cfg.PropCfg;
 import org.azolla.l.ling.util.Log0;
 
 import java.util.Map;
+import java.util.ResourceBundle;
 
 /**
  * The coder is very lazy, nothing to write for this class
@@ -20,40 +21,10 @@ import java.util.Map;
  */
 public class I18N
 {
-    public static PropCfg propCfg = null;
-
-    public static Map<String, String> propMap = Maps.newHashMap();
-
-    static
-    {
-        try
-        {
-            PropCfg.cfg("/i18n.Azolla.Cfg.properties");
-        }
-        catch (Exception e)
-        {
-            Log0.error(I18N.class, e.toString(), e);
-        }
-        propMap.put("title", "2048 Ver1.0.9");
-    }
+    public static PropCfg propCfg = PropCfg.cfg("i18n.2048.p.Azolla.Cfg.properties");;
 
     public static String get(String key)
     {
-        try
-        {
-            return propCfg.get(key);
-        }
-        catch (Exception e)
-        {
-            return propMap.get(key);
-        }
-    }
-
-    public static void main(String[] args)
-    {
-        for(Map.Entry<String, String> entry : propMap.entrySet())
-        {
-            System.out.println(entry.getKey()+"="+entry.getValue());
-        }
+        return propCfg.get(key);
     }
 }
